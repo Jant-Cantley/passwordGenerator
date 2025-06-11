139164cb41c57ce07907cd2679b5a14c3a74198b
ENOCK JUMA <40110521+BoqinReese@users.noreply.github.com>
2025-06-11 08:49:16 +0200

    Fixed the "Old password not disappearing" problem
    Modified the onclick function. Check the comments for details.

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-let generateBtn = document.getElementById("generate-btn")
-let firstPasswordEl = document.getElementById("password-first")
-let secondPasswordEl = document.getElementById("password-second")
-
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"]
 
 
 
-generateBtn.addEventListener("click", function() {
+function generatePassword(){
+    let firstPasswordEl = document.getElementById("password-first")
+    let secondPasswordEl = document.getElementById("password-second")
+    let passwordOne = [] //using an empty list as a placeholder for the random password.
+    let passwordTwo = []
+
      for ( i = 0; i < 15; i++) {
-        let passwordOne = Math.floor(Math.random() * characters.length)
-        let passwordTwo = Math.floor(Math.random() * characters.length)
-        firstPasswordEl.textContent += characters[passwordOne]
-        secondPasswordEl.textContent += characters[passwordTwo]
+        //Created a random number, used it as to get the digit in the characters list above then pushed it to the empty placeholder list
+        passwordOne.push(characters[Math.floor(Math.random() * characters.length)])
+        passwordTwo.push(characters[Math.floor(Math.random() * characters.length)])
     }
-})
+    //The generated password will be separated by comma, so the join("") will remove the commas
+    firstPasswordEl.textContent = passwordOne.join("")
+    secondPasswordEl.textContent = passwordTwo.join("")
+
+
+    // If u didn't understand something here let me know!!!
+}
